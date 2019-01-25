eea8f5995c7756175447f4b0d7bbbbe9942ba09f
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-25 11:07:42 -0500

    challenge 3 completed

--- src/com/theironyard/BankAccount.java
+++ src/com/theironyard/BankAccount.java
@@ -10,11 +10,38 @@ public class BankAccount {
         this.balance = initialBalance;
     }
 
+    //one way to make thread safe
+
+//    public synchronized void deposit(double amount) {
+//        balance += amount;
+//    }
+//
+//    public synchronized void withdraw(double amount) {
+//        balance -= amount;
+//    }
+
+    //better way to make thread safe
+    //you do not need to synchronize anything else
+    //over-synchronization can have a noticeably negative impact on performance
+
     public void deposit(double amount) {
-        balance += amount;
+        synchronized (this) {
+            balance += amount;
+        }
     }
 
     public void withdraw(double amount) {
-        balance -= amount;
+        synchronized (this) {
+            balance -= amount;
+        }
     }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void printAccountNumber() {
+        System.out.println("Account number = " + accountNumber);
+    }
+
 }
