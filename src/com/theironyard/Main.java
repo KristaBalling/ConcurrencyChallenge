d276288e9293c0f2fc93a59df66c9b3f986ecd08
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-25 10:57:18 -0500

    added BankAccount class

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -3,7 +3,7 @@ package com.theironyard;
 public class Main {
 
     public static void main(String[] args) {
-        BankAccount account = new BankAccount("12345-678", 1000.00);
+        final BankAccount account = new BankAccount("12345-678", 1000.00);
 
 //        Thread trThread1 = new Thread() {
 //            public void run() {
@@ -18,6 +18,22 @@ public class Main {
 //                account.withdraw(100.00);
 //            }
 //        };
+
+        Thread trThread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                account.deposit(300);
+                account.withdraw(50.00);
+            }
+        });
+
+        Thread trThread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                account.deposit(203.75);
+                account.withdraw(100.00);
+            }
+        });
 
         trThread1.start();
         trThread2.start();
