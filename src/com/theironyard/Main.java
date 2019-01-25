1182adbb4a52460e9e39bb8fea218fea06046582
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-25 10:49:28 -0500

    initial commit

---
+++ src/com/theironyard/Main.java
@@ -0,0 +1,25 @@
+package com.theironyard;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount("12345-678", 1000.00);
+
+//        Thread trThread1 = new Thread() {
+//            public void run() {
+//                account.deposit(300);
+//                account.withdraw(50.00);
+//            }
+//        };
+//
+//        Thread trThread2 = new Thread() {
+//            public void run() {
+//                account.deposit(203.75);
+//                account.withdraw(100.00);
+//            }
+//        };
+
+        trThread1.start();
+        trThread2.start();
+    }
+}
